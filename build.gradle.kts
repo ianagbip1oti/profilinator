cbd9d518e46017c2a85ef4f7ee6a3186b5f2fbe0
ianagbip1oti <ianagbip1oti@gmail.com>
2020-12-09 03:06:09 +0000

    📦 NEW: Switch to sqlite

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://jitpack.io")
 }
@@ -16,7 +17,7 @@ dependencies {
     implementation("com.apollographql.apollo:apollo-runtime:2.4.4")
     implementation("com.github.boscojared.disparse:disparse-smalld:master-SNAPSHOT")
     implementation("com.github.princesslana:smalld:0.2.6")
-    implementation("com.github.princesslana:somedb:0.1.0")
+    implementation("com.github.princesslana:somedb:0.3.0")
     implementation("com.google.guava:guava:28.0-jre")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.squareup.okhttp3:okhttp:3.12.11")
